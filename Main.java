143ea8da1de18785d06875b081af9d45fd0b1c12
João Victor <ra46791342838@acaonsfatima.org.br>
2024-11-08 18:15:10 -0300

    só os bglhs básicos

--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        
+        double pequeno = 13.5;
+        byte medio = 15;
+        double grande = 17.5;
+
+        var qtdPequenos = pequeno*4;
+        var qtdMedio = medio*4;
+        var qtdgrande = grande*4;
+
+        var somaFinal = qtdPequenos+ qtdMedio+ qtdgrande;
+
+        System.out.println("a soma final é de: " +somaFinal);
+
     }
 }
